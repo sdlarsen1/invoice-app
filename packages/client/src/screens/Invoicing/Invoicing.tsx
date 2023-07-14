31afb62a6403c185ecb0f81be375f156d1fe2bc0
Stephen Larsen <stephen.larsen@meetgradient.com>
2023-07-13 20:59:58 -0700

    create invoicing main screen

---
+++ packages/client/src/screens/Invoicing/Invoicing.tsx
@@ -0,0 +1,26 @@
+import AppBarPortal from "components/AppBar/AppBarPortal";
+import React from "react";
+
+const Invoicing = () => {
+  return (
+    <>
+      <AppBarPortal />
+      <header className="App-header">
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </>
+  );
+};
+
+export default Invoicing;
